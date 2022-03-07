1262482c9d94dce08d6c1188c73345e159068ee9
Zane Birkett <axelbirkett@gmail.com>
2022-03-07 10:24:04 -0400

    Buttons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Typography, Button, Container } from "@material-ui/core";
 
 export default function Create() {
   return (
-    <div>
+    <Container>
       <Typography
         variant="h6"
         component="h2"
@@ -12,6 +12,23 @@ export default function Create() {
       >
         Create a new note
       </Typography>
-    </div>
+      <Button
+        onClick={() => {
+          console.log("you clicked me");
+        }}
+        type="submit"
+        color="secondary"
+        variant="contained"
+      >
+        Submit
+      </Button>
+
+      {/* <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+        <Button>Four</Button>
+      </ButtonGroup> */}
+    </Container>
   );
 }
