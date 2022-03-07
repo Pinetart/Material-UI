24b4fb23d8d87461e238149b7eaa0971efdeba74
Zane Birkett <axelbirkett@gmail.com>
2022-03-07 11:29:28 -0400

    makeStyles Hook

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { Typography, Button, Container } from "@material-ui/core";
+import { Typography, Button, Container } from "@material-ui/core"; //Components
 import PublishIcon from "@material-ui/icons/Publish";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import { makeStyles } from "@material-ui/core"; //Functions
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: "violet",
+    "&:hover": {
+      backgroundColor: "blue",
+    },
+  },
+  title: {
+    textDecoration: "underline",
+    marginBottom: 20,
+  },
+});
 
 export default function Create() {
+  const classes = useStyles();
   return (
     <Container>
       <Typography
+        className={classes.title}
         variant="h6"
         component="h2"
         gutterBottom
@@ -15,6 +32,7 @@ export default function Create() {
         Create a new note
       </Typography>
       <Button
+        className={classes.btn}
         onClick={() => {
           console.log("you clicked me");
         }}
