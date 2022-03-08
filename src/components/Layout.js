6b3216b87a5ce90cb1b85737ae44c7cb89e01677
Zane Birkett <axelbirkett@gmail.com>
2022-03-08 15:41:28 -0400

    App Bar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,30 +1,44 @@
-import { Drawer, makeStyles, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Drawer,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
 import { AddCircleOutlined, SubjectOutlined } from "@material-ui/icons";
 import {
   useHistory,
   useLocation,
 } from "react-router-dom/cjs/react-router-dom.min";
+import { format } from "date-fns";
 const drawerWidth = 240;
 
-const useStyles = makeStyles({
-  page: {
-    background: "#f9f9f9",
-    width: "100%",
-    paddingTop: 30,
-    paddingBottom: 30,
-  },
-  drawer: { width: drawerWidth },
-  drawerPaper: { width: drawerWidth },
-  root: { display: "flex" },
-  active: {
-    background: "#D3D3D3",
-  },
-  title: {
-    textAlign: "center",
-    paddingTop: 15,
-    PaddingBottom: 30,
-  },
+const useStyles = makeStyles((theme) => {
+  return {
+    page: {
+      background: "#f9f9f9",
+      width: "100%",
+      paddingTop: 30,
+      paddingBottom: 30,
+    },
+    drawer: { width: drawerWidth },
+    drawerPaper: { width: drawerWidth },
+    root: { display: "flex" },
+    active: {
+      background: "#D3D3D3",
+    },
+    title: {
+      textAlign: "center",
+      paddingTop: 15,
+      PaddingBottom: 30,
+    },
+    appbar: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+    toolbar: theme.mixins.toolbar,
+    date: { flexGrow: 1 },
+  };
 });
 
 const Layout = ({ children }) => {
@@ -47,7 +61,7 @@ const Layout = ({ children }) => {
 
   return (
     <div className={classes.root}>
-      {/* app bar */}
+      {/* Left Drawer */}
       <Drawer
         className={classes.drawer}
         variant="permanent"
@@ -77,8 +91,20 @@ const Layout = ({ children }) => {
           ))}
         </List>
       </Drawer>
-      {/* side drawer */}
-      <div className={classes.page}>{children}</div>
+      {/* App Bar */}
+      <AppBar elevation={0} className={classes.appbar} color="text.primary">
+        <Toolbar>
+          <Typography className={classes.date}>
+            Today is the {format(new Date(), "do MMMM Y")}
+          </Typography>
+          <Typography>Mario</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <div className={classes.page}>
+        <div className={classes.toolbar}></div>
+        {children}
+      </div>
     </div>
   );
 };
