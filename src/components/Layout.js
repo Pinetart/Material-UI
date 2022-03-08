3d558084ff41cf47864e15d42c609b206f9a7202
Zane Birkett <axelbirkett@gmail.com>
2022-03-08 13:30:32 -0400

    Layout Component

---
+++ src/components/Layout.js
@@ -0,0 +1,21 @@
+import { makeStyles } from "@material-ui/core";
+const useStyles = makeStyles({
+  page: {
+    background: "#f9f9f9",
+    width: "100%",
+  },
+});
+
+const Layout = ({ children }) => {
+  const classes = useStyles();
+  return (
+    <div>
+      {/* app bar */}
+
+      {/* side drawer */}
+      <div className={classes.page}>{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
